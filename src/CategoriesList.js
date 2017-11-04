4f99efedb0e2118fde6996c7a599b0f022dd4295
ryukit <ryukit.rk@gmail.com>
2017-11-04 12:42:12 +0200

    linear db try

--- src/CategoriesList.js
+++ src/CategoriesList.js
@@ -1,6 +1,51 @@
 import React, { Component } from 'react';
 import CategoryItem from './CategoryItem';
 
+const tree = [
+	{id: 'c-01', name: 'Cat 1', sub: ['c-01'], parentId: null},
+	{id: 'c-02', name: 'Cat 2', sub: [], parentId: null},
+	{id: 'c-03', name: 'Cat 3', sub: [], parentId: null},
+	{id: 'c-04', name: 'Cat 4', sub: [], parentId: null},
+	{id: 'c-05', name: 'Cat 1.1', sub: ['c-06'], parentId: 'c-01'},
+	{id: 'c-06', name: 'Cat 1.1.1', sub: [], parentId: 'c-05'}
+];
+
+const Categories = ({ tree, source }) => {
+	debugger;
+	return (
+		<ul>
+			{
+				tree.map(cat => 
+					<CategoryItemRener key={cat.id} {...cat} source={tree}/>)
+			}
+		</ul>
+	)
+};
+
+function getCatById(source, id) {
+	for ( let i = 0; i < source.length; i++ ) {
+		if (source[i].id === id) {
+			return source[i];
+		}
+	}
+}
+
+const CategoryItemRener = ({id, name, sub, source}) => {
+	const shouldRenderChildren = !!sub.length;
+	const subTree = sub.map(id => getCatById(source, id) )
+	debugger;
+	return (
+		<li>
+			{name}
+			{
+				shouldRenderChildren && (
+					<Categories tree={ subTree } />
+				)	
+			}
+		</li>
+	)
+};
+
 class CategoriesList extends Component {
 	render() {
 		let sendDeleteCategory=this.props.sendDeleteCategory;
@@ -54,6 +99,7 @@ class CategoriesList extends Component {
 	                <div className="col s12">
 	                    {categoryItem}
 	                </div>
+	                <Categories tree={tree} source={tree} />
 	            </div>
 	        );
 	    }
